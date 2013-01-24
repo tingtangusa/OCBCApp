e03fa9efa11a1e148466e70ab9a4d0bc3503eb2b
jsyeo <jasonyeo88@gmail.com>
2013-01-24 22:17:59 +0800

    added services and branch spinner to appointments form

--- src/com/jason/ocbcapp/AppointmentsFragment.java
+++ src/com/jason/ocbcapp/AppointmentsFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AppointmentsFragment extends Fragment {
 
@@ -13,11 +15,32 @@ public class AppointmentsFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Setup spinner for branch form item
+        Spinner branchesSpinner = (Spinner) getView().findViewById(
+                R.id.branchesSpinner);
+        ArrayAdapter<CharSequence> branchesAdapter = ArrayAdapter
+                .createFromResource(getView().getContext(), R.array.branches,
+                        android.R.layout.simple_spinner_dropdown_item);
+        branchesSpinner.setAdapter(branchesAdapter);
+        
+        // Setup spinner for services requested form item
+        Spinner servicesSpinner = (Spinner) getView().findViewById(
+                R.id.serviceSpinner);
+        ArrayAdapter<CharSequence> servicesAdapter = ArrayAdapter
+                .createFromResource(getView().getContext(), R.array.services,
+                        android.R.layout.simple_spinner_dropdown_item);
+        servicesSpinner.setAdapter(servicesAdapter);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View myFragmentView = inflater.inflate(R.layout.fragmentlayout,
-                container, false);
+        View apptView = inflater.inflate(R.layout.fragmentlayout, container,
+                false);
 
-        return myFragmentView;
+        return apptView;
     }
 }
