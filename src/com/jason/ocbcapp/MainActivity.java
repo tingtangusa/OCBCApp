be46c1c9ff22cc0c87e21b5bb36be11897972c05
jsyeo <jasonyeo88@gmail.com>
2013-03-21 14:02:25 +0800

    remove dummy fragment

--- src/com/jason/ocbcapp/MainActivity.java
+++ src/com/jason/ocbcapp/MainActivity.java
@@ -371,6 +371,8 @@ public class MainActivity extends SherlockFragmentActivity implements
                 return new NotificationsFragment();
             case 2:
                 return new AppointmentsFragment();
+            default:
+                return new Fragment();
             }
         }
 
@@ -390,33 +392,6 @@ public class MainActivity extends SherlockFragmentActivity implements
                 return getString(R.string.title_section_appts).toUpperCase();
             }
             return null;
-        }
-    }
-
-    /**
-     * A dummy fragment representing a section of the app, but that simply
-     * displays dummy text.
-     */
-    public static class DummySectionFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        public DummySectionFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            // Create a new TextView and set its text to the fragment's section
-            // number argument value.
-            TextView textView = new TextView(getActivity());
-            textView.setGravity(Gravity.CENTER);
-            textView.setText(Integer.toString(getArguments().getInt(
-                    ARG_SECTION_NUMBER)));
-            return textView;
         }
     }
 
