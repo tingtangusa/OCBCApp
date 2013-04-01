0e006964f7662bdd72bfe68499517218a61c866f
jsyeo <jasonyeo88@gmail.com>
2013-04-01 14:56:19 +0800

    added new layout and adapter for nearest branches

---
+++ src/com/jason/ocbcapp/BranchLocationAdapter.java
@@ -0,0 +1,82 @@
+package com.jason.ocbcapp;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class BranchLocationAdapter extends BranchAdapter {
+
+    protected static final String APP_TAG = MainActivity.APP_TAG;
+    ArrayList<Pair<String, Double>> branchesWithDistance;
+
+    public BranchLocationAdapter(Activity activity, ArrayList<Pair<String, Double>> branchesWithDistance) {
+        super(activity, getBranchesNames(branchesWithDistance));
+        this.branchesWithDistance = branchesWithDistance;
+    }
+
+    private static ArrayList<String> getBranchesNames(
+            ArrayList<Pair<String, Double>> branchesWithDistance) {
+        ArrayList<String> branchesNames = new ArrayList<String>();
+        for (Pair<String, Double> pair : branchesWithDistance) {
+            branchesNames.add(pair.first);
+        }
+        return branchesNames;
+    }
+
+    /*
+     * (non-Javadoc)
+     * Some optimization employed here. For more information,
+     * @see com.jason.ocbcapp.BranchAdapter#getView(int, android.view.View, android.view.ViewGroup)
+     */
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.branches_location_row, null);
+
+            holder = new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(R.id.branchName);
+            holder.distance = (TextView) convertView.findViewById(R.id.branchDistance);
+            holder.btn = (Button) convertView.findViewById(R.id.waitingTimeBtn);
+            holder.pb = (ProgressBar) convertView
+                    .findViewById(R.id.progressBar);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.pb.setVisibility(View.INVISIBLE);
+
+        String branchName = branchesWithDistance.get(position).first;
+        holder.name.setText(branchName);
+
+        long distance = Math.round(branchesWithDistance.get(position).second) / 1000;
+        holder.distance.setText(distance + " km");
+
+        int buttonState = waitingTimes[position];
+        if (buttonState != -1) {
+            holder.btn.setText(buttonState + "mins");
+        } else {
+            holder.btn.setText("");
+        }
+        holder.btn.setTag(position);
+        holder.btn.setOnClickListener(btnClickListener);
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView name;
+        TextView distance;
+        Button btn;
+        ProgressBar pb; 
+    }
+}
